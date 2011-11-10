12b27de51a2bc62198629a45082a55dc619e9a8e
Arthur Schreiber <schreiber.arthur@googlemail.com>
2011-11-10 17:54:54 +0100

    Extend the ChangeMatcher.

--- lib/spec/matchers/change_matcher.tcl
+++ lib/spec/matchers/change_matcher.tcl
@@ -7,30 +7,57 @@ namespace eval Spec {
             if { [dict exists $args by] } {
                 my set expected_delta [dict get $args by]
             }
+
+            if { [dict exists $args by_at_most] } {
+                my set maximum [dict get $args by_at_most]
+            }
+
+            if { [dict exists $args by_at_least] } {
+                my set minimum [dict get $args by_at_least]
+            }
+
+            if { [dict exists $args from ] } {
+                my set expected_before [dict get $args from]
+            }
+
+            if { [dict exists $args to ] } {
+                my set expected_after [dict get $args to]
+            }
+
+        }
+
+        ChangeMatcher instproc change_expected? { } {
+            expr { ![my exists expected_delta] || [my set expected_delta] != 0 }
         }
 
         ChangeMatcher instproc matches? { actual } {
             my set actual $actual
 
-            my set actual_before [uplevel [Matcher set eval_level] [my set expected]]
-            uplevel [Matcher set eval_level] $actual
-            my set actual_after [uplevel [Matcher set eval_level] [my set expected]]
+            my set actual_before [uplevel [::Spec::Matchers set eval_level] [my set expected]]
+            uplevel [::Spec::Matchers set eval_level] $actual
+            my set actual_after [uplevel [::Spec::Matchers set eval_level] [my set expected]]
 
-            expr { [my changed?] && [my matches_expected_delta?] }
-        }
-
-        ChangeMatcher instproc does_not_match? { actual } {
-            my set actual $actual
-
-            my set actual_before [uplevel [Matcher set eval_level] [my set expected]]
-            uplevel [Matcher set eval_level] $actual
-            my set actual_after [uplevel [Matcher set eval_level] [my set expected]]
-
-            expr { ![my changed?] || ![my matches_expected_delta?] }
+            expr { (![my change_expected?] || [my changed?]) && [my matches_expected_delta?] && [my matches_max?] && [my matches_min?] && [my matches_before?] && [my matches_after?] }
         }
 
         ChangeMatcher instproc matches_expected_delta? { } {
             expr { [my exists expected_delta] ? [my actual_delta] == [my set expected_delta] : true }
+        }
+
+        ChangeMatcher instproc matches_max? { } {
+            expr { [my exists maximum] ? [my actual_delta] <= [my set maximum] : true }
+        }
+
+        ChangeMatcher instproc matches_min? { } {
+            expr { [my exists minimum] ? [my actual_delta] >= [my set minimum] : true }
+        }
+
+        ChangeMatcher instproc matches_before? { } {
+            expr { [my exists expected_before] ? [my set actual_before] == [my set expected_before] : true}
+        }
+
+        ChangeMatcher instproc matches_after? { } {
+            expr { [my exists expected_after] ? [my set actual_after] == [my set expected_after] : true}
         }
 
         ChangeMatcher instproc actual_delta {} {
@@ -42,11 +69,23 @@ namespace eval Spec {
         }
 
         ChangeMatcher instproc positive_failure_message {} {
-            if { [my exists expected_delta] } {
-                return "{[my set expected]} should have been changed by <[my set expected_delta]>, but was changed by <[my actual_delta]>"
+            if { ![my matches_before?] } {
+                return "result should have been initially been '[my set expected_before]', but was '[my set actual_before]'"
+            } elseif { ![my matches_after?] } {
+                return "result should have been changed to '[my set expected_after]', but is now '[my set actual_after]'"
+            } elseif { [my exists expected_delta] } {
+                return "result should have been changed by '[my set expected_delta]', but was changed by '[my actual_delta]'"
+            } elseif { [my exists maximum] } {
+                return "result should have been changed by at most '[my set maximum]', but was changed by '[my actual_delta]'"
+            } elseif { [my exists minimum] } {
+                return "result should have been changed by at least '[my set minimum]', but was changed by '[my actual_delta]'"
             } else {
-                return "{[my set expected]} should have changed, but is still <[my set actual_before]>"
+                return "result should have changed, but is still '[my set actual_before]'"
             }
+        }
+
+        ChangeMatcher instproc negative_failure_message {} {
+            return "result should not have changed, but did change from '[my set actual_before]' to '[my set actual_after]'"
         }
     }
 }
---
+++ spec/matchers/change_matcher_spec.tcl
@@ -0,0 +1,142 @@
+lappend auto_path [file join [file dirname [info script]] ".." ".." "lib"]
+package require spec/autorun
+
+describe "expect to change, with a numeric value" {
+    before each {
+        set value 0
+    }
+
+    it "passes if the actual value is modified by the block" {
+        expect { incr value } to change { set value }
+    }
+
+    it "fails if the actual value is not modified by the block" {
+        expect {
+            expect {} to change { set value }
+        } to raise_error -code EXPECTATION_NOT_MET -message "result should have changed, but is still '0'"
+    }
+}
+
+describe "expect to not change, with a numeric value" {
+    before each {
+        set value 0
+    }
+
+    it "passes if the actual value is not modified by the block" {
+        expect {} to not change { set value }
+    }
+
+    it "fails if the actual value is modified by the block" {
+        expect {
+            expect { incr value } to not change { set value }
+        } to raise_error -code EXPECTATION_NOT_MET -message "result should not have changed, but did change from '0' to '1'"
+    }
+}
+
+describe "expect to not change, with a list" {
+    before each {
+        set value {}
+    }
+
+    it "passes if the actual value is modified by the block" {
+        expect { lappend value "test" } to change { set value }
+    }
+
+    it "fails if the actual value is not modified by the block" {
+        expect {
+            expect {} to change { set value }
+        } to raise_error -code EXPECTATION_NOT_MET -message "result should have changed, but is still ''"
+    }
+}
+
+describe "expect to not change, with a numeric value" {
+    before each {
+        set value {}
+    }
+
+    it "passes if the actual value is not modified by the block" {
+        expect {} to not change { set value }
+    }
+
+    it "fails if the actual value is modified by the block" {
+        expect {
+            expect { lappend value "test" } to not change { set value }
+        } to raise_error -code EXPECTATION_NOT_MET -message "result should not have changed, but did change from '' to 'test'"
+    }
+}
+
+describe "expect to change by" {
+    before each {
+        set value 0
+    }
+
+    it "passes if the actual value is changed by the expected amount" {
+        expect { incr value 4 } to change { set value } by 4
+    }
+
+    it "passes if the actual value is not changed and the expected amount is 0" {
+        expect { incr value 0 } to change { set value } by 0
+    }
+
+    it "fails if the actual value is changed by an unexpected amount" {
+        expect {
+            expect { incr value 2 } to change { set value } by 4
+        } to raise_error -code EXPECTATION_NOT_MET -message "result should have been changed by '4', but was changed by '2'"
+    }
+}
+
+describe "expect to change by_at_most" {
+    before each {
+        set value 0
+    }
+
+    it "passes if the actual value is changed by less than the expected amount" {
+        expect { incr value 2 } to change { set value } by_at_most 4
+    }
+
+    it "passes if the actual value is changed by the expected amount" {
+        expect { incr value 4 } to change { set value } by_at_most 4
+    }
+
+    it "fails if the actual value is changed by more than the expected amount" {
+        expect {
+            expect { incr value 5 } to change { set value } by_at_most 4
+        } to raise_error -code EXPECTATION_NOT_MET -message "result should have been changed by at most '4', but was changed by '5'"
+    }
+}
+
+describe "expect to change by_at_least" {
+    before each {
+        set value 0
+    }
+
+    it "passes if the actual value is changed by less than the expected amount" {
+        expect { incr value 5 } to change { set value } by_at_least 4
+    }
+
+    it "passes if the actual value is changed by the expected amount" {
+        expect { incr value 4 } to change { set value } by_at_least 4
+    }
+
+    it "fails if the actual value is changed by less than the expected amount" {
+        expect {
+            expect { incr value 2 } to change { set value } by_at_least 4
+        } to raise_error -code EXPECTATION_NOT_MET -message "result should have been changed by at least '4', but was changed by '2'"
+    }
+}
+
+describe "expect to change from" {
+    before each {
+        set value "string"
+    }
+
+    it "passes when the actual value is equal to the expected value before executing the block" {
+        expect { set value "other string" } to change { set value } from "string"
+    }
+
+    it "fails when the actual value is not equal to the expected value before executing the block" {
+        expect {
+            expect { set value "foo" } to change { set value } from "bar"
+        } to raise_error -code EXPECTATION_NOT_MET -message "result should have been initially been 'bar', but was 'string'"
+    }
+}
