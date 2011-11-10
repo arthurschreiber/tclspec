71d8f2ea0b5b5c0841ab20eb732fe4d1f5ba124e
Arthur Schreiber <schreiber.arthur@googlemail.com>
2011-11-10 17:53:58 +0100

    Extend the RaiseErrorMatcher.

--- lib/spec/matchers/raise_error_matcher.tcl
+++ lib/spec/matchers/raise_error_matcher.tcl
@@ -1,14 +1,76 @@
 namespace eval Spec {
     namespace eval Matchers {
         Class RaiseErrorMatcher -superclass BaseMatcher
-        RaiseErrorMatcher instproc init { args } {
+        RaiseErrorMatcher instproc init { {-code NONE} -message } {
+            my set expected_code $code
 
+            if { [info exists message]} {
+                my set expected_message $message
+            }
         }
 
         RaiseErrorMatcher instproc matches? { actual } {
             next
-            set rc [catch [list uplevel [Matcher set eval_level] $actual] value]
-            expr { $rc == 1 }
+
+            my set raised_expected_code false
+            my set with_expected_message false
+
+            set rc [catch [list uplevel [::Spec::Matchers set eval_level] $actual] message options]
+
+            if { $rc != 1 && $rc != 2 } {
+                return false
+            }
+
+            my set actual_code $::errorCode
+            my set actual_message $message
+            if { $rc == 2 } {
+                my set actual_code [dict get $options -errorcode]
+            }
+
+
+            my set with_expected_message [my verify_message $message]
+
+            if { [my set expected_code] == "NONE" || [my set expected_code] == [my set actual_code] } {
+                my set raised_expected_code true
+            }
+
+            if { [my set raised_expected_code] && [my set with_expected_message] } {
+                return true
+            } else {
+                return false
+            }
+        }
+
+        RaiseErrorMatcher instproc verify_message { message } {
+            if { [my exists expected_message ]} {
+                expr { [my set expected_message] == $message }
+            } else {
+                return true
+            }
+        }
+
+        RaiseErrorMatcher instproc positive_failure_message { } {
+            return "expected [my expected_error][my given_error]"
+        }
+
+        RaiseErrorMatcher instproc negative_failure_message { } {
+            return "expected no [my expected_error][my given_error]"
+        }
+
+        RaiseErrorMatcher instproc expected_error {} {
+            if { [my exists expected_message ]} {
+                return "error with code '[my set expected_code]' and message '[my set expected_message]'"
+            } else {
+                return "error with code '[my set expected_code]'"
+            }
+        }
+
+        RaiseErrorMatcher instproc given_error {} {
+            if { [my exists actual_code ]} {
+                return ", got error with code '[my set actual_code]' and message '[my set actual_message]'"
+            } else {
+                return " but nothing was raised"
+            }
         }
     }
 }
---
+++ spec/matchers/raise_error_matcher_spec.tcl
@@ -0,0 +1,66 @@
+lappend auto_path [file join [file dirname [info script]] ".." ".." "lib"]
+package require spec/autorun
+
+proc throw_some_error {} {
+    return -code error -errorcode SOME_ERROR
+}
+
+describe "expect to raise_error" {
+    it "passes if an error is raised" {
+        expect { error "error!" } to raise_error
+    }
+
+    it "fails if nothing is raised" {
+        expect {
+            expect {} to raise_error
+        } to raise_error -code EXPECTATION_NOT_MET -message "expected error with code 'NONE' but nothing was raised"
+    }
+}
+
+describe "expect to raise_error -message" {
+    it "passes if an error is raised with the right message" {
+        expect { error "error message" } to raise_error -message "error message"
+    }
+
+    it "fails if an error is raised with the wrong message" {
+        expect {
+            expect { error "wrong message" } to raise_error -message "error message"
+        } to raise_error -code EXPECTATION_NOT_MET -message "expected error with code 'NONE' and message 'error message', got error with code 'NONE' and message 'wrong message'"
+    }
+}
+
+describe "expect to raise_error -code" {
+    it "passes if the expected error code is raised" {
+        expect {
+            throw_some_error
+        } to raise_error -code SOME_ERROR
+    }
+
+    it "fails if nothing is raised" {
+        expect {
+            expect {} to raise_error -code SOME_ERROR
+        } to raise_error -code EXPECTATION_NOT_MET -message "expected error with code 'SOME_ERROR' but nothing was raised"
+    }
+
+    it "fails if another error code is raised" {
+        expect {
+            expect { throw_some_error } to raise_error -code OTHER_ERROR
+        } to raise_error -code EXPECTATION_NOT_MET -message "expected error with code 'OTHER_ERROR', got error with code 'SOME_ERROR' and message ''"
+    }
+}
+
+describe "expect to not raise_error -code" {
+    it "passes if nothing is raised" {
+        expect { } to not raise_error -code SOME_ERROR
+    }
+
+    it "passes if another error is raised" {
+        expect { throw_some_error } to not raise_error -code OTHER_ERROR
+    }
+
+    it "fails if the passed error code is raised" {
+        expect {
+            expect { throw_some_error } to not raise_error -code SOME_ERROR
+        } to raise_error -code EXPECTATION_NOT_MET -message "expected no error with code 'SOME_ERROR', got error with code 'SOME_ERROR' and message ''"
+    }
+}
