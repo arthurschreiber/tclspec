d00c184f9fc33b8ced942ca0fc0d4533bfec4f72
Arthur Schreiber <schreiber.arthur@gmail.com>
2011-11-14 23:05:04 +0100

    Adding more failing examples.

--- spec/example_group_spec.tcl
+++ spec/example_group_spec.tcl
@@ -43,6 +43,25 @@ source [file join [file dirname [info script]] "spec_helper.tcl"]
 #     }
 # }
 
+describe "child" {
+    it "is known by its parent" {
+        set parent [ExampleGroup describe]
+        set child [$parent describe]
+        expect [$parent children] to equal [list $child]
+    }
+
+    it "is not registered in the world" {
+        set world [World new]
+        set parent [ExampleGroup describe]
+
+        $world register $parent
+
+        $parent describe
+
+        expect [$world example_groups] to equal [list $parent]
+    }
+}
+
 describe "before, after, and around hooks" {
     it "runs the before alls in order" {
         set group [ExampleGroup describe]
@@ -98,5 +117,80 @@ describe "before, after, and around hooks" {
         $group run [NullObject new]
 
         expect $::order to equal [list 3 2 1]
+    }
+}
+
+describe "adding examples" {
+    it "allows adding examples using 'it'" {
+        set group [ExampleGroup describe]
+        $group it "should do something" { }
+        expect [llength [$group examples]] to equal 1
+    }
+
+    it "exposes all added examples using 'examples'" {
+        set group [ExampleGroup describe]
+        $group it "should do something 1" { }
+        $group it "should do something 2" { }
+        $group it "should do something 3" { }
+        expect [llength [$group examples]] to equal 3
+    }
+
+    it "maintains the example order" {
+        set group [ExampleGroup describe]
+        $group it "should 1" { }
+        $group it "should 2" { }
+        $group it "should 3" { }
+
+        expect [[lindex [$group examples] 0] description] to equal "should 1"
+        expect [[lindex [$group examples] 1] description] to equal "should 2"
+        expect [[lindex [$group examples] 2] description] to equal "should 3"
+    }
+}
+
+describe "running the examples" {
+    it "returns true if all examples pass" {
+        set group [ExampleGroup describe "group" {
+            example "ex 1" {
+                expect 1 to equal 1
+            }
+
+            example "ex 2" {
+                expect 1 to equal 1
+            }
+        }]
+
+        expect [$group run] to be true
+    }
+
+    it "returns false if any of the examples fail" {
+        set group [ExampleGroup describe "group" {
+            example "ex 1" {
+                expect 1 to equal 1
+            }
+
+            example "ex 2" {
+                expect 1 to equal 2
+            }
+        }]
+
+        expect [$group run] to be false
+    }
+
+    it "runs all examples, regardless of failing ones" {
+        set group [ExampleGroup describe "group" {
+            example "ex 1" {
+                expect 1 to equal 2
+            }
+
+            example "ex 2" {
+                expect 1 to equal 1
+            }
+        }]
+
+        for example [$group examples] {
+            $example should_receive "run"
+        }
+
+        expect [$group run] to be false
     }
 }
