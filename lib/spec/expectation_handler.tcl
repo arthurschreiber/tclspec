85c5a0894c5f4aaa00a4e71ad5af07dcca7c6349
Arthur Schreiber <schreiber.arthur@googlemail.com>
2011-11-16 12:37:21 +0100

    Better backtraces for expectation failures.

--- lib/spec/expectation_handler.tcl
+++ lib/spec/expectation_handler.tcl
@@ -4,7 +4,18 @@ namespace import xotcl::*
 namespace eval Spec {
     Class Expectations
     Expectations proc fail_with { message } {
-        return -code error -errorcode "EXPECTATION_NOT_MET" $message
+        # Setting the return level here allows us to tweak generated backtraces
+        # for expectation failures a bit. Thanks to this, the backtrace will
+        # state that an error occurred during execution of "expect ...",
+        # instead of during "Expectations fail_with ...".
+        #
+        # Also, this keeps the backtrace shorter and more focused, as we're
+        # usually not concerned with what happens between the call to "expect"
+        # and the generation of the expectation failure.
+        #
+        # TODO: The return level here is currently hard-coded, but it should be
+        #       possible to determine the correct level programmatically.
+        return -level 4 -code error -errorcode "EXPECTATION_NOT_MET" $message
     }
 
     Class PositiveExpectationHandler
