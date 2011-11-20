8fa1c26cbd8ede55c731547202b94a4004975109
Arthur Schreiber <schreiber.arthur@googlemail.com>
2011-11-20 11:00:29 +0100

    Add specs for be_within and be =~ matchers.

---
+++ spec/matchers/be_within_matcher_spec.tcl
@@ -0,0 +1,78 @@
+lappend auto_path [file join [file dirname [info script]] ".." ".."]
+package require spec/autorun
+
+source [file join [file dirname [info script]] ".." "spec_helper.tcl"]
+
+describe "expect <actual> to be within <delta> of <expected>" {
+    it "passes when actual == expected" {
+        expect 5.0 to be_within 0.5 of 5.0
+    }
+
+    it "passes when actual < (expected + delta)" {
+        expect 5.49 to be_within 0.5 of 5.0
+    }
+
+    it "passes when actual > (expected - delta)" {
+        expect 4.51 to be_within 0.5 of 5.0
+    }
+
+    it "fails when actual == (expected - delta)" {
+        expect {
+            expect 4.5 to be_within 0.5 of 5.0
+        } to fail_with ""
+    }
+
+    it "fails when actual < (expected - delta)" {
+        expect {
+            expect 4.49 to be_within 0.5 of 5.0
+        } to fail_with ""
+    }
+
+    it "fails when actual == (expected + delta)" {
+        expect {
+            expect 5.5 to be_within 0.5 of 5.0
+        } to fail_with ""
+    }
+
+    it "fails when actual > (expected + delta)" {
+        expect {
+            expect 5.51 to be_within 0.5 of 5.0
+        } to fail_with ""
+    }
+}
+
+describe "expect <actual> to not be within <delta> of <expected>" {
+    it "passes when actual == (expected - delta)" {
+        expect 4.5 to not be_within 0.5 of 5.0
+    }
+
+    it "passes when actual < (expected - delta)" {
+        expect 4.49 to not be_within 0.5 of 5.0
+    }
+
+    it "passes when actual == (expected + delta)" {
+        expect 5.5 to not be_within 0.5 of 5.0
+    }
+
+    it "passes when actual > (expected + delta)" {
+        expect 5.51 to not be_within 0.5 of 5.0
+    }
+
+    it "fails when actual == expected" {
+        expect {
+            expect 5.0 to not be_within 0.5 of 5.0
+        } to fail_with ""
+    }
+
+    it "fails when actual < (expected + delta)" {
+        expect {
+            expect 5.49 to not be_within 0.5 of 5.0
+        } to fail_with ""
+    }
+
+    it "fails when actual > (expected - delta)" {
+        expect {
+            expect 4.51 to not be_within 0.5 of 5.0
+        } to fail_with ""
+    }
+}
---
+++ spec/matchers/list_match_matcher.tcl
@@ -0,0 +1,58 @@
+lappend auto_path [file join [file dirname [info script]] ".." ".."]
+package require spec/autorun
+
+source [file join [file dirname [info script]] ".." "spec_helper.tcl"]
+
+describe "expect <actual> to be =~ <expected>" {
+    it "passes if actual and expected contain the same items" {
+        expect [list 1 2 3] to be =~ [list 1 2 3]
+    }
+
+    it "passes if actual and expected contain the same items in different order" {
+        expect [list 1 3 2] to be =~ [list 1 2 3]
+    }
+
+    it "fails if actual includes extra items" {
+        expect {
+            expect [list 1 2 3 4] to be =~ [list 1 2 3]
+        } to fail_with ""
+    }
+
+    it "fails if actual misses items" {
+        expect {
+            expect [list 1 2] to be =~ [list 1 2 3]
+        } to fail_with ""
+    }
+
+    it "fails if actual misses items and includes extra items" {
+        expect {
+            expect [list 1 2 4] to be =~ [list 1 2 3]
+        } to fail_with ""
+    }
+
+    it "sorts items in the error message" {
+        expect {
+            expect [list 6 2 1 5] to be =~ [list 4 1 2 3]
+        } to fail_with ""
+    }
+
+    it "accurately reports extra elements when there are duplicates" {
+        expect {
+            expect [list 1 1 1 5] to =~ [list 1 5]
+        } to fail_with ""
+    }
+
+    it "accurately reports missing elements when there are duplicates" {
+        expect {
+            expect [list 1 5] to be =~ [list 1 1 5]
+        } to fail_with ""
+    }
+}
+
+describe "expect <actual> to not be =~ <expected>" {
+    it "is not supported" {
+        expect {
+            expect [list 1 2 3] to not be =~ [list 1 2 3]
+        } to fail_with ""
+    }
+}
