fb50d7d713cbd5c3547b683fc719b3cfa7e7deff
Arthur Schreiber <schreiber.arthur@gmail.com>
2011-12-07 22:33:17 +0100

    Sketch out some Mock functionality.

--- lib/spec/mocks/mock.tcl
+++ lib/spec/mocks/mock.tcl
@@ -3,5 +3,54 @@ namespace eval Spec {
         namespace path ::Spec
 
         Class create Mock
+        Mock instproc init { name } {
+            my set name $name
+            my set expected [list]
+            my set called [list]
+        }
+
+        Mock instproc should_receive { method_name -with -and_return } {
+            my lappend expected $method_name
+
+            if { [info exists and_return] } {
+                my proc $method_name { args } "
+                    my lappend called $method_name
+                    apply {$and_return} {*}\$args
+                "
+            } else {
+                my proc $method_name { args } "
+                    my lappend called $method_name
+                    return
+                "
+            }
+        }
+
+        Mock instproc should_not_receive { method_name -with } {
+            if { [info exists with] } {
+                my proc $method_name { args } "
+                    puts \"Checking \$args against {$with}\"
+                    puts \"it returns \[expr {\$args == {$with} } ]\"
+                    if { \$args == {$with} } {
+                        return -code error -errorcode MockExpectationError \"bla\"
+                    }
+                "
+            } else {
+                my proc $method_name { args } "
+                    return -code error -errorcode MockExpectationError \"bla\"
+                "
+            }
+        }
+
+        Mock instproc spec_verify { } {
+            set verified true
+            foreach exp [my set expected] {
+                set verified [expr { $verified && $exp in [my set called] }]
+            }
+            return $verified
+        }
+
+        Mock instproc spec_reset { } {
+
+        }
     }
 }
--- spec/mocks/mock_spec.tcl
+++ spec/mocks/mock_spec.tcl
@@ -3,101 +3,63 @@ package require spec/autorun
 
 source [file join [file dirname [info script]] ".." "spec_helper.tcl"]
 
-namespace eval Spec::Mocks {}
-
-
-Class create MethodsMixin
-MethodsMixin instproc should_receive { } {
-    # Add a message expectation
-}
-
-MethodsMixin instproc should_not_receive { proc } {
-    # Add a negative message expectation
-}
-
-MethodsMixin instproc stub { proc } {
-    # Add a stub
-}
-
-MethodsMixin instproc unstub { proc } {
-    # Remove a stub
-}
-
-MethodsMixin instproc spec_verify { } {
-    # Verify and reset the mock
-}
-
-MethodsMixin instproc spec_reset { } {
-    # Reset the Mock
-}
-
-
-Class create Spec::Mocks::Mock
-
-::Spec::Mocks::Mocks instmixin MethodsMixin
-::Spec::Mocks::Mock instproc init { description } {
-    my set description $description
-}
-
-::Spec::Mocks::Mock instproc spec_verify { } {
-    my set description $description
-}
-
-Class create MethodDouble
-MethodDouble instproc init { object method_name } {
-    my set object $object
-    my set method_name $method_name
-}
-
-MethodDouble instproc setup { } {
-    # Add the object to the Mock Space,
-    # call store_original_method and install_mock_method
-}
-
-MethodDouble instproc store_original_method { } {
-    # Check whether the object has the passed proc defined
-    # as an object proc.
-    # If it is, we have to stash it.
-    # If it doesn't, there's nothing to do
-}
-
-MethodDouble instproc install_mock_method { } {
-    # Replace the method with something that notifies
-    # the mock proxy that the method was called
-}
-
-MethodDouble instproc restore_original_method { } {
-    # Check whether the method was stashed and restore it if it was
-}
-
-MethodDouble instproc verify { } {
-    # Go through the expectations and
-}
-
-MethodDouble instproc reset { } {
-    # Restore the original method and clear
-}
-
-MethodDouble instproc clear { } {
-    # Remove stubs and expectations
-}
-
 describe "::Spec::Mocks::Mock" {
     before each {
-        my set mock [::Spec::Mocks::Mock new "test double"]
+        set mock [::Spec::Mocks::Mock new "test double"]
+    }
+
+    after each {
+        $mock spec_reset
     }
 
     it "passes when not receiving message specified as not to be received" {
-        my instvar mock
         $mock should_not_receive "not_expected"
         $mock spec_verify
     }
 
+    it "passes when not receiving message specified as not to be received with different args" {
+        $mock should_not_receive "message" -with {"unwanted text"}
+        $mock message "other text"
+        $mock spec_verify
+    }
+
     it "fails when receiving message specified as not to be received" {
-        my instvar mock
         $mock should_not_receive "not_expected"
         expect {
             $mock not_expected
-        } to raise_error -code MockExpectationError -message "(Double 'test double').not_expected(no args)\n    expected: 0 times\n    received: 1 time"
+        } to raise_error -code MockExpectationError
+    }
+
+    it "fails when receiving message specified as not to be received with args" {
+        $mock should_not_receive "not_expected" -with "unexpected text"
+        expect {
+            $mock not_expected "unexpected text"
+        } to raise_error -code MockExpectationError
+    }
+
+    it "allows blocks to calculate return values" {
+        $mock should_receive "something" -with {"a" "b" "c"} -and_return { {a b c} {
+            join [list $a $b $c] ""
+        } }
+
+        expect [$mock something "a" "b" "c"] to equal "abc"
+        $mock spec_verify
+    }
+
+    it "returns the previously stubbed value if no return value was set" {
+        $mock stub "something" -with "a" "b" "c" -and_return { {a b c} {
+            join [list $a $b $c] ""
+        } }
+        $mock should_receive "something" -with {"a" "b" "c"}
+
+        expect [$mock something "a" "b" "c"] to equal "abc"
+        $mock spec_verify
+    }
+
+    it "returns nothing if no return value is set and there is no previously stubbed value" {
+        $mock should_receive "something" -with {"a" "b" "c"}
+
+        expect [$mock something "a" "b" "c"] to equal ""
+        $mock spec_verify
     }
 }
