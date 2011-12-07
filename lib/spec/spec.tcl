484cb63d6b6f5914cfdcc94a83be6725df82a788
Arthur Schreiber <arthur.schreiber@hp.com>
2011-12-07 09:10:55 +0100

    Switch the toplevel ::Spec Class to NX.

--- lib/spec/spec.tcl
+++ lib/spec/spec.tcl
@@ -10,15 +10,6 @@ namespace eval Spec {
     namespace import ::at_exit::*
 }
 
-::xotcl::Class create Spec
-Spec proc world {} {
-    if { ![::xotcl::my exists world ] } {
-        ::xotcl::my set world [Spec::World new]
-    }
-
-    ::xotcl::my set world
-}
-
 source [file join [file dirname [info script]] "dsl.tcl"]
 source [file join [file dirname [info script]] "example.tcl"]
 source [file join [file dirname [info script]] "example_group.tcl"]
@@ -27,3 +18,8 @@ source [file join [file dirname [info script]] "matchers.tcl"]
 source [file join [file dirname [info script]] "reporter.tcl"]
 source [file join [file dirname [info script]] "runner.tcl"]
 source [file join [file dirname [info script]] "world.tcl"]
+
+nx::Class create Spec {
+    :require namespace
+    :class property [list world [::Spec::World new]]
+}
