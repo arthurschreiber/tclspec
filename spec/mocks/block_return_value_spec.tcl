3867e5bb7bfc27fd7cc05cdacb543f950c23707c
Arthur Schreiber <schreiber.arthur@gmail.com>
2012-02-29 17:43:12 +0100

    Add a spec that tests passing a block to should_receive / stub.

---
+++ spec/mocks/block_return_value_spec.tcl
@@ -0,0 +1,17 @@
+describe "A double declaration that is passed an implementation block" {
+    describe "using should_receive" {
+        it "returns the value of executing the block" {
+            set obj [::Spec::Mocks::Mock new]
+            $obj should_receive "foo" {{} { return "bar" }}
+            expect [$obj foo] to equal "bar"
+        }
+    }
+
+    describe "using stub" {
+        it "returns the value of executing the block" {
+            set obj [::Spec::Mocks::Mock new]
+            $obj stub "foo" {{} { return "bar" }}
+            expect [$obj foo] to equal "bar"
+        }
+    }
+}
