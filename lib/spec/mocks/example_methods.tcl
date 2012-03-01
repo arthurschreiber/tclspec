779acddc1b966a5d5f6cd001ca764361b1ad4ca8
Arthur Schreiber <schreiber.arthur@gmail.com>
2012-03-01 15:56:09 +0100

    Make tcl mocking methods available inside examples.

--- lib/spec/mocks/example_methods.tcl
+++ lib/spec/mocks/example_methods.tcl
@@ -13,7 +13,23 @@ namespace eval Spec {
                 declare_double "Stub" {*}$args
             }
 
-            namespace export double mock stub
+            # Replace a tcl command with a custom implementation.
+            proc stub_call { args } {
+                ::Spec::Mocks::Tcl::Doubler stub_call {*}$args
+            }
+
+            # Replace a tcl command with a custom implementation and a
+            # call expectation.
+            proc mock_call { args } {
+                ::Spec::Mocks::Tcl::Doubler mock_call {*}$args
+            }
+
+            # Replace a tcl command with a negative call expectation.
+            proc dont_call { args } {
+                ::Spec::Mocks::Tcl::Doubler dont_call {*}$args
+            }
+
+            namespace export double mock stub stub_call mock_call dont_call
 
             proc declare_double { declared_as {name ""} args } {
                 ::Spec::Mocks::nx::Mock new -name $name \
