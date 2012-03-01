89255abe45294da27d6e57729ef3db6b8094cdbf
Arthur Schreiber <schreiber.arthur@gmail.com>
2012-03-01 12:42:19 +0100

    Add some Tcl mock specs.

---
+++ spec/mocks/tcl/any_number_of_times_spec.tcl
@@ -0,0 +1,39 @@
+describe "A Tcl Mock expectation with -any_number_of_times" {
+    before each {
+        set doubler [::Spec::Mocks::TclDoubler]
+        $doubler mock_call "::random_call" -any_number_of_times
+    }
+
+    it "passes if the method is not called" {
+        expect {
+            $doubler spec_verify
+        } to not raise_error
+    }
+
+    it "passes if the method is called once" {
+        ::random_call
+
+        expect {
+            $doubler spec_verify
+        } to not raise_error
+    }
+
+    it "passes if the method is called multiple times" {
+        for {set i 0} {$i < 10} {incr i} {
+            ::random_call
+        }
+
+        expect {
+            $doubler spec_verify
+        } to not raise_error
+    }
+
+    it "is preferred to stubs on the same method" {
+        $doubler stub_call "::random_call" -and_return "stub_value"
+
+        $doubler mock_call "::random_call" -any_number_of_times -and_return "mock_value"
+
+        expect [::random_call] to equal "mock_value"
+        expect [::random_call] to equal "mock_value"
+    }
+}
---
+++ spec/mocks/tcl/multiple_return_values_spec.tcl
@@ -0,0 +1,56 @@
+source [file join [file dirname [info script]] ".." ".." "spec_helper.tcl"]
+
+describe "A Tcl mock expectation with multiple return values and no specified count" {
+    before each {
+        set doubler [::Spec::Mocks::TclDoubler]
+        set return_values [list 1 "2" [nx::Object new]]
+
+        $doubler mock_call "::message" -and_return $return_values
+    }
+
+    after each {
+        [$doubler new] spec_reset
+    }
+
+    it "returns values in order to consecutive calls" {
+        expect [::message] to equal [lindex $return_values 0]
+        expect [::message] to equal [lindex $return_values 1]
+        expect [::message] to equal [lindex $return_values 2]
+        [::Spec::Mocks::TclDoubler new] spec_verify
+    }
+
+    it "complains when there are too few calls" {
+        expect [::message] to equal [lindex $return_values 0]
+        expect [::message] to equal [lindex $return_values 1]
+
+        expect {
+            [::Spec::Mocks::TclDoubler new] spec_verify
+        } to raise_error -code ::Spec::Mocks::ExpectationError \
+          -message "::message(any args)\n    expected: 3 times\n    received: 2 times"
+    }
+
+    it "complains when there are too many calls" {
+        expect [::message] to equal [lindex $return_values 0]
+        expect [::message] to equal [lindex $return_values 1]
+        expect [::message] to equal [lindex $return_values 2]
+        expect [::message] to equal [lindex $return_values 2]
+
+        expect {
+            [::Spec::Mocks::TclDoubler new] spec_verify
+        } to raise_error -code ::Spec::Mocks::ExpectationError \
+            -message "::message(any args)\n    expected: 3 times\n    received: 4 times"
+    }
+
+    it "doesn't complain when there are too many calls but the method was stubbed" {
+        $doubler stub_call "::message" -and_return "stub_result"
+
+        expect [::message] to equal [lindex $return_values 0]
+        expect [::message] to equal [lindex $return_values 1]
+        expect [::message] to equal [lindex $return_values 2]
+        expect [::message] to equal "stub_result"
+
+        expect {
+            [::Spec::Mocks::TclDoubler new] spec_verify
+        } to not raise_error -code ::Spec::Mocks::ExpectationError
+    }
+}
