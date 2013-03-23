93e966b97cfe3453b86b6fda2806914ada5ad72c
Arthur Schreiber <arthur.schreiber@hp.com>
2013-03-23 22:23:22 +0000

    Convert Spec::World from NX to TclOO.

--- lib/spec/world.tcl
+++ lib/spec/world.tcl
@@ -1,29 +1,29 @@
-package require nx
+package require "TclOO" "1.0"
 
 namespace eval Spec {
-    nx::Class create World {
-        :variable example_groups [list]
+    oo::class create World {
+        variable example_groups [list]
 
-        :public method example_groups {} {
-            set :example_groups
+        method example_groups {} {
+            set [self]::example_groups
         }
 
-        :public method register { example_group } {
-            lappend :example_groups $example_group
+        method register { example_group } {
+            lappend [self]::example_groups $example_group
             return $example_group
         }
 
-        :public method example_count { } {
+        method example_count { } {
             set count 0
 
-            foreach group ${:example_groups} {
+            foreach group [set [self]::example_groups] {
                 incr count [llength [$group examples]]
             }
 
             return $count
         }
 
-        :public method run_hooks { hook context example_group_instance } {
+        method run_hooks { hook context example_group_instance } {
             [Spec configuration] run_hooks $hook $context $example_group_instance
         }
     }
